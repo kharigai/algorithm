f039d56cc4df13598e8279b888e46c2bfae230e4
Kazuhiro Harigai <kharigai@gmail.com>
2022-01-06 22:23:27 +0900

    New create binary_search.py

---
+++ python/binary_search.py
@@ -0,0 +1,10 @@
+from typing import List, NewType
+
+IndexNumber = NewType('IndexNumber', int)
+
+def binary_search(numbers: List[int], value: int) -> IndexNumber:
+    pass
+
+if __name__ == '__main__':
+    nums = [i for i in range(10)]
+    print(nums)
